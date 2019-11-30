c2004cc735b531d1ad38fdce3ef5ffccb219fcda
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2019-11-30 16:35:55 +0000

    Add index file to export all modules

---
+++ index.js
@@ -0,0 +1,6 @@
+export * from './src/js/retrieve-cookies';
+export * from './src/js/check-cookie-exists';
+export * from './src/js/delete-cookie';
+export * from './src/js/purge-cookies';
+export * from './src/js/set-cookies';
+export * from './src/js/show-banner';
