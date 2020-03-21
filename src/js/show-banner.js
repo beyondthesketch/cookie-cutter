6f298e05782c8f54fbfaba6a41ae609d7f8de7a1
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2020-03-21 12:03:52 +0000

    Banner markup updates, handle reject cookies

--- src/js/show-banner.js
+++ src/js/show-banner.js
@@ -2,7 +2,7 @@ import { setCookie } from './set-cookies.js';
 import { purgeCookies } from './purge-cookies.js';
 import { checkCookieExists } from './check-cookie-exists.js';
 
-const cookieName = 'btsCookieControl';
+const cookieName = 'btsCookies';
 
 export const showBanner = (
     settings = {}
@@ -12,19 +12,22 @@ export const showBanner = (
         linkText = 'Find out more',
         text = 'This site uses cookies and similar technologies to help make it work better. By continuing, you agree to our cookie policy',
         confirmText = 'Accept',
-        declineText = 'No, thanks',
+        declineText = 'Reject',
         title = 'Use of cookies',
-        necessaryCookiesTitle = 'Necessary Cookies',
+        necessaryCookiesTitle,
         necessaryCookiesText = 'These cookies are required to enable core functionality such as security, network management and accessibility. You may disable these by changing your browser settings. Disabling these cookies may prevent use of the website\'s full functionality',
         optionalCookies
     } = settings;
 
-    if (!link || checkCookieExists(cookieName)) {
+    if (checkCookieExists(cookieName) || !link) {
         return false;
     }
     const bannerBody = document.createElement('aside');
+    bannerBody.className = 'bts-cookie-banner';
     const bannerTitle = document.createElement('h1');
+    bannerTitle.className = 'bts-cookie-banner__title';
     const textP = document.createElement('p');
+    textP.className = 'bts-cookie-banner__text';
 
     if (!!title) {
         bannerTitle.textContent = title;
@@ -34,33 +37,59 @@ export const showBanner = (
     bannerBody.appendChild(textP);
 
     const linkA = document.createElement('a');
+    linkA.className = 'bts-cookie-banner__link';
+    linkA.setAttribute('href', link);
     linkA.textContent = linkText;
     bannerBody.appendChild(linkA);
 
     if (necessaryCookiesTitle && necessaryCookiesText) {
         const requiredCookieContainer = document.createElement('div');
+        requiredCookieContainer.className = 'bts-cookie-banner__req-cookies';
 
         const requiredCookieTitle = document.createElement('h2');
+        requiredCookieTitle.className = 'bts-cookie-banner__req-cookies-title';
         requiredCookieTitle.textContent = necessaryCookiesTitle;
         requiredCookieContainer.appendChild(requiredCookieTitle);
 
         const requiredCookieText = document.createElement('p');
+        requiredCookieText.className = 'bts-cookie-banner__req-cookies-text';
         requiredCookieText.textContent = necessaryCookiesText;
         requiredCookieContainer.appendChild(requiredCookieText);
 
         bannerBody.appendChild(requiredCookieContainer);
     }
 
+    const buttonWrapper = document.createElement('div');
+    buttonWrapper.className = 'bts-cookie-banner__buttons';
+
 
     const acceptBttn = document.createElement('button');
+    acceptBttn.className = 'bts-cookie-banner__button m--accept-button';
     acceptBttn.textContent = confirmText;
     acceptBttn.addEventListener('click', () => setCookie(cookieName, 'true', 365));
-    bannerBody.appendChild(acceptBttn);
+
+    buttonWrapper.appendChild(acceptBttn);
 
     const declineBttn = document.createElement('button');
+    declineBttn.className = 'bts-cookie-banner__button m--decline-button';
     declineBttn.textContent = declineText;
-    declineBttn.addEventListener('click', purgeCookies);
-    bannerBody.appendChild(declineBttn);
+    declineBttn.addEventListener(
+        'click', () => {
+            purgeCookies();
+            setCookie(
+                cookieName,
+                'false',
+                365
+            );
+            document.body.removeChild(bannerBody);
+        }
+    );
+
+    buttonWrapper.appendChild(declineBttn);
+
+    bannerBody.appendChild(buttonWrapper);
 
     document.body.appendChild(bannerBody);
+
+    return bannerBody;
 };
