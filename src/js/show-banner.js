754538a493eeb327b9446986cab87c3babd6deb5
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2020-03-21 13:31:36 +0000

    WIP implement optional cookies

--- src/js/show-banner.js
+++ src/js/show-banner.js
@@ -1,3 +1,5 @@
+// import moveIn from '@beyondthesketch/scriptuccinojs/modules/fx/moveIn';
+
 import { setCookie } from './set-cookies.js';
 import { purgeCookies } from './purge-cookies.js';
 import { checkCookieExists } from './check-cookie-exists.js';
@@ -11,9 +13,11 @@ export const showBanner = (
         link,
         linkText = 'Find out more',
         text = 'This site uses cookies and similar technologies to help make it work better. By continuing, you agree to our cookie policy',
-        confirmText = 'Accept',
-        declineText = 'Reject',
+        confirmText = 'Accept all',
+        declineText = 'Reject all',
         title = 'Use of cookies',
+        manageFeature,
+        manageText = 'Manage cookies',
         necessaryCookiesTitle,
         necessaryCookiesText = 'These cookies are required to enable core functionality such as security, network management and accessibility. You may disable these by changing your browser settings. Disabling these cookies may prevent use of the website\'s full functionality',
         optionalCookies
@@ -24,6 +28,8 @@ export const showBanner = (
     }
     const bannerBody = document.createElement('aside');
     bannerBody.className = 'bts-cookie-banner';
+    bannerBody.setAttribute('id', 'bts-cookie-banner');
+    bannerBody.setAttribute('data-bts-cookie-banner', '');
     const bannerTitle = document.createElement('h1');
     bannerTitle.className = 'bts-cookie-banner__title';
     const textP = document.createElement('p');
@@ -42,6 +48,14 @@ export const showBanner = (
     linkA.textContent = linkText;
     bannerBody.appendChild(linkA);
 
+    const manageDetails = document.createElement('details');
+    manageDetails.className = 'bts-cookie-banner__manage';
+    const manageSummary = document.createElement('summary');
+    manageSummary.textContent = manageText;
+    manageDetails.appendChild(manageSummary);
+
+    bannerBody.appendChild(manageDetails);
+
     if (necessaryCookiesTitle && necessaryCookiesText) {
         const requiredCookieContainer = document.createElement('div');
         requiredCookieContainer.className = 'bts-cookie-banner__req-cookies';
@@ -56,7 +70,39 @@ export const showBanner = (
         requiredCookieText.textContent = necessaryCookiesText;
         requiredCookieContainer.appendChild(requiredCookieText);
 
-        bannerBody.appendChild(requiredCookieContainer);
+        manageDetails.appendChild(requiredCookieContainer);
+    }
+
+    // TODO: These are categories of cookies which should include the cookies to set - not individual cookies
+    if (manageFeature && optionalCookies && Array.isArray(optionalCookies) && optionalCookies.length) {
+        optionalCookies.forEach(
+            (cookie) => {
+                const {
+                    title,
+                    description,
+                    cookieList
+                } = cookie;
+
+                const wrapper = document.createElement('div');
+                wrapper.className = 'bts-cookie-banner__opt-cookies';
+                const catTitle = document.createElement('h2');
+                catTitle.className = 'bts-cookie-banner__opt-title';
+                catTitle.textContent = title;
+                wrapper.appendChild(catTitle);
+                const catDesc = document.createElement('p');
+                catDesc.className = 'bts-cookie-banner__opt-description';
+                catDesc.textContent = description;
+                wrapper.appendChild(catDesc);
+
+                const catCheckbox = document.createElement('input');
+                catCheckbox.className = 'bts-cookie-banner__opt-check';
+                catCheckbox.setAttribute('type', 'checkbox');
+                catCheckbox.setAttribute('data-id', title);
+                wrapper.appendChild(catCheckbox);
+
+                manageDetails.appendChild(wrapper);
+            }
+        )
     }
 
     const buttonWrapper = document.createElement('div');
@@ -66,7 +112,17 @@ export const showBanner = (
     const acceptBttn = document.createElement('button');
     acceptBttn.className = 'bts-cookie-banner__button m--accept-button';
     acceptBttn.textContent = confirmText;
-    acceptBttn.addEventListener('click', () => setCookie(cookieName, 'true', 365));
+    acceptBttn.addEventListener(
+        'click',
+        () => {
+            setCookie(
+                cookieName,
+                JSON.stringify({c:1, d: Date.now()}),
+                365
+            );
+            document.body.removeChild(document.getElementById('bts-cookie-banner'));
+        }
+    );
 
     buttonWrapper.appendChild(acceptBttn);
 
@@ -78,10 +134,10 @@ export const showBanner = (
             purgeCookies();
             setCookie(
                 cookieName,
-                'false',
+                '0',
                 365
             );
-            document.body.removeChild(bannerBody);
+            document.body.removeChild(document.getElementById('bts-cookie-banner'));
         }
     );
 
